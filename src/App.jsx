465cdafa7ffd624b67be9456e3ab7c5a604d710c
Charles Bruno <charlesbrcosta@gmail.com>
2024-07-02 14:26:23 -0300

    feat: Added GlobalStyles component to normlize css and configured vite.config.js to customize component import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { styled } from 'styled-components';
+import { GlobalStyles } from '@components/GlogalStyles';
+
+
+const GradientBackground = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`
+
 export function App() {
 
   return (
-    <>
-    </>
+    <GradientBackground>
+      <GlobalStyles />
+    </GradientBackground>
   )
 }
